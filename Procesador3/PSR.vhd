c8394f851057fcdd61f5a57243c3cece25670718
angelik loaiza <angelica.loaiza323@gmail.com>
2016-11-01 17:04:38 -0500

    procesador3
    Se inicia desde cero al notar que los anteriores habia tenido errores,
    para comprender y entender mejor personalmente.

---
+++ Procesador3/PSR.vhd
@@ -0,0 +1,31 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity PSR is
+    Port ( nzvc : in  STD_LOGIC_VECTOR (3 downto 0);
+			  clk : in  STD_LOGIC;
+			   rst : in  STD_LOGIC;
+           Carry : out  STD_LOGIC);
+end PSR;
+
+architecture Behavioral of PSR is
+
+signal PSR: std_logic_vector(31 downto 0):= (others=>'0');
+
+begin
+
+process(clk,nzvc)
+	begin
+	if(rising_edge(clk))then
+			if(rst = '1') then				
+				carry <= '0';
+			else
+				PSR(23 downto 20) <= nzvc;
+				Carry <= PSR(20);
+			end if;
+		end if;
+end process;
+end Behavioral;
+
